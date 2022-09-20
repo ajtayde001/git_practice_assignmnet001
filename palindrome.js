872b711945f1fddcd12055d6570eb4fe3f6c6978
ajtayde001 <ajtayde001@gmail.com>
2022-09-20 17:58:52 +0530

    added both program

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+ 
+ function checkPalindrome(str){
+let new_str="";
+
+for(let i=str.length-1;i>=0; i--){
+  new_str=new_str+str_new[i]
+}
+if(str==new_str){
+  console.log("palindrome");
+}else{
+  console.log("not palindrome");
+}
+}
+checkPalindrome("nayan")
---
+++ prime.js
@@ -0,0 +1,15 @@
+ function checkPrime(num){
+
+let count=0; 
+ for(let i=1 ;i<=num; i++){
+  if(num%i0){
+     count++;
+   }
+ }
+ if(count=2){
+   console.log(num,"is prime number")
+}else{
+   console.log(num,"is not prime number")
+ }
+}
+ checkPrime(29);
